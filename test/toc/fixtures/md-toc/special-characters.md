7d393f51b3678f105d76eefb92249a26ab031c01
Evgeniy Gavryushin <job.egavr@yandex.ru>
2014-12-11 16:22:25 +0300

    Update tests

---
+++ test/toc/fixtures/md-toc/special-characters.md
@@ -0,0 +1,30 @@
+<!--TOC-->
+* <a href="#h1">h1</a>
+* <a href="#href">!@#$%</a>
+* <a href="#href-1">???</a>
+* <a href="#--">???  !!!</a>
+* <a href="#h">^([#{h$</a>
+* <a href="#h-1">/\\h/\\</a>
+          * <a href="#-blah"># blah</a>
+
+
+<a name="h1"></a>
+#h1#############
+
+<a name="href"></a>
+# !@#$%#
+
+<a name="href-1"></a>
+# ???
+
+<a name="--"></a>
+# ???  !!!
+
+<a name="h"></a>
+# ^([#{h$
+
+<a name="h-1"></a>
+#/\h/\
+
+<a name="-blah"></a>
+####### blah
--- test/toc/toc.js
+++ test/toc/toc.js
@@ -24,8 +24,73 @@ describe('TOC generation', function () {
         });
     });
 
+    it('must not generate a TOC', function (done) {
+        var files = readFiles('no-toc');
+
+        toc(files.md, function (err, res) {
+            if (err) {
+                done(err);
+            } else {
+                res.must.be.equal(files['md-toc']);
+                done();
+            }
+        });
+    });
+
     it('must generate a TOC with duplicate headers\' names', function (done) {
         var files = readFiles('duplicate');
+
+        toc(files.md, function (err, res) {
+            if (err) {
+                done(err);
+            } else {
+                res.must.be.equal(files['md-toc']);
+                done();
+            }
+        });
+    });
+
+    it('must work \'ignoreFirstHeader\' option', function (done) {
+        var files = readFiles('ignore-first-header');
+
+        toc(files.md, { ignoreFirstHeader: true }, function (err, res) {
+            if (err) {
+                done(err);
+            } else {
+                res.must.be.equal(files['md-toc']);
+                done();
+            }
+        });
+    });
+
+    it('must work \'maxDepth\' option', function (done) {
+        var files = readFiles('max-depth');
+
+        toc(files.md, { maxDepth: 3 }, function (err, res) {
+            if (err) {
+                done(err);
+            } else {
+                res.must.be.equal(files['md-toc']);
+                done();
+            }
+        });
+    });
+
+    it('must handle headers with \'special characters\'', function (done) {
+        var files = readFiles('special-characters');
+
+        toc(files.md, function (err, res) {
+            if (err) {
+                done(err);
+            } else {
+                res.must.be.equal(files['md-toc']);
+                done();
+            }
+        });
+    });
+
+    it('must handle invalid structure of headers', function (done) {
+        var files = readFiles('invalid-structure');
 
         toc(files.md, function (err, res) {
             if (err) {
