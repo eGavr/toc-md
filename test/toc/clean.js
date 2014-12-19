0330d2aee63f3d20524af0ec1ca73662b41b4eda
Evgeniy Gavryushin <job.egavr@yandex.ru>
2014-12-19 11:57:32 +0300

    Refactor tests

--- test/toc/clean.js
+++ test/toc/clean.js
@@ -15,58 +15,6 @@ describe('TOC clean', function () {
         });
     });
 
-    it('must clean a nested TOC', function (done) {
-        var files = readFiles('nested');
-
-        toc.clean(files['toc-md'], function (err, res) {
-            if (err) {
-                done(err);
-            } else {
-                res.must.be.equal(files.md);
-                done();
-            }
-        });
-    });
-
-    it('must clean a russian TOC', function (done) {
-        var files = readFiles('ru');
-
-        toc.clean(files['toc-md'], function (err, res) {
-            if (err) {
-                done(err);
-            } else {
-                res.must.be.equal(files.md);
-                done();
-            }
-        });
-    });
-
-    it('must clean a TOC with duplicate headers\' names', function (done) {
-        var files = readFiles('duplicate');
-
-        toc.clean(files['toc-md'], function (err, res) {
-            if (err) {
-                done(err);
-            } else {
-                res.must.be.equal(files.md);
-                done();
-            }
-        });
-    });
-
-    it('must clean a TOC with different headers\' types', function (done) {
-        var files = readFiles('different-types');
-
-        toc.clean(files['toc-md'], function (err, res) {
-            if (err) {
-                done(err);
-            } else {
-                res.must.be.equal(files.md);
-                done();
-            }
-        });
-    });
-
     it('must not clean a TOC', function (done) {
         var files = readFiles('no-toc');
 
